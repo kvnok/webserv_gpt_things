1cc3ce0f66ff6d8df2712a82ff17e0ff82c548c1
Kevin Kroon <kkroon@student.codam.nl>
2024-05-01 11:51:06 +0200

    exe permissions on bin

--- compile_stuff.sh
+++ compile_stuff.sh
@@ -8,4 +8,5 @@ for file in ${src_dir}/*.cpp; do
     filename=$(basename -- "$file")
     filename_no_ext=bin/"${filename%.*}"
     g++ -o "${filename_no_ext}" "${file}" -lpthread
+    chmod +x "${filename_no_ext}"
 done
